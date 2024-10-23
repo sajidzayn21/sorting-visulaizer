a712167d8aa56c1185f5f5b6b2f1d218471b761a
sajidzayn21 <sajidkhalidzayn@gmail.com>
2024-10-23 21:43:19 +0530

    Initial SetUp for Sorting Visulaizer Project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      
+    },
+  },
+  safelist: ["bg-green-800", "bg-yellow-600", "bg-orange-700", "bg-red-800", "bg-lime-700"],
+  plugins: [],
+};
